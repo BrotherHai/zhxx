06c401415b4b68c292fe8e7deda2bddd5b654c9d
BrotherHai <50613992+BrotherHai@users.noreply.github.com>
2022-02-12 00:04:07 +0800

    Update cj.js

--- cj.js
+++ cj.js
@@ -1,9 +1,6 @@
 var request = require('request');
 var  options = {　　　　　　　method: 'post',
             url: 'http://cj.liuxiaohai.xyz/index.php?s=/Admin/Index/caiji',
-            headers: {
-              'Content-Type': 'application/x-www-form-urlencoded'
-            }
           };
 request(options, function (err, res, body) {
             if (err) {
